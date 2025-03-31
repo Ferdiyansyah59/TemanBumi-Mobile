1645f820caa7087950fb576f7fdeb661c196971b
Ferdiyansyah59 <ferdir5599@gmail.com>
2025-03-31 17:15:31 +0700

    refactor: config

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -1,6 +1,13 @@
 // const AI_API =
-const AI_API = 'http://82.112.230.106:5001';
-const API_URL = 'http://82.112.230.106:8018/api';
-const ARTICLE_IMAGE = 'http://82.112.230.106:8859/storage/public/';
+//   'https://sampah-lagi-done-please-3lqrspa3va-et.a.run.app/sampah';
+const AI_API = 'https://sampahml.anitech.id';
+const API_URL = 'https://sampahbe.anitech.id/api';
+// const AI_API = 'https://carbons-api-3lqrspa3va-et.a.run.app/carbons';
+const ARTICLE_IMAGE = 'https://temanbumi.anitech.id/storage/';
+// const AI_API =
+//   'https://sampah-lagi-done-please-3lqrspa3va-et.a.run.app/sampah';
+// const API_URL = 'https://sampah-go-3lqrspa3va-et.a.run.app/api';
+// const AI_API = 'https://carbons-api-3lqrspa3va-et.a.run.app/carbons';
+// const ARTICLE_IMAGE = 'https://admin-temanbumi.ferdirns.com/storage/';
 
 export { AI_API, API_URL, ARTICLE_IMAGE };
